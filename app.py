36128221294ecc35778d62b1a493e694c182814c
DeepakRautella <95517385+DeepakRautella@users.noreply.github.com>
2022-06-20 16:44:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app=Flask(__name__)
 
-@app.route("/",methods=["GET","POST"])
+@app.route("/",methods=["GET"])
 def index():
     return"Starting Machine Learning Project"
 
