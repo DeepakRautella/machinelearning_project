f032fd4aed3a40bc8be717069013124c6ae3d9ce
DeepakRautella <deepak.rautella@gmail.com>
2022-06-25 05:29:02 +0530

    update_docker_app

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
 
+from flask import Flask
+import sys
+from housing.logger import logging
+from housing.exception import HousingException
 app=Flask(__name__)
 
-@app.route("/",methods=["GET","POST"])
+
+@app.route("/",methods=['GET','POST'])
 def index():
-    return"Starting Machine Learning Project"
+    try:
+        raise Exception("We are testing custom exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("We are testing logging module")
+    return "CI CD pipeline has been established."
 
 
 if __name__=="__main__":
